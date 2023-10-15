296c8ae32d56287b8425860d9a20c315bf8e8f6e
Olawale-S <olawalearcee@gmail.com>
2023-10-15 01:06:36 +0100

    setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  theme: {
+    screens: {
+      xs: "300px",  //360px
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      primary: "#32D583",
+
+      white: "#fff",
+      black:  "#000"
+    },
+    
+  },
+  plugins: [],
+}
+
